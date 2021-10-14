650915be247664ec31d7cfe26f16357393bf2418
souadouabid <souad.ouabid@estudiantat.upc.edu>
2021-10-14 20:18:45 +0200

    Cambio de pantalla login a registro

--- app/src/main/java/com/app/login/MainActivity.java
+++ app/src/main/java/com/app/login/MainActivity.java
@@ -1,7 +1,12 @@
 package com.app.login;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ButtonRegistrar = (Button) findViewById(R.id.buttonRegistrar);
+        Button ButtonEntrar = (Button) findViewById(R.id.buttonEntrar);
+
+        ButtonRegistrar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, Registro.class));
+            }
+        });
+        EditText TextLogin = (EditText) findViewById(R.id.editTextUser);
+        EditText Textpassword = (EditText) findViewById(R.id.editTextPassword);
+        ButtonEntrar.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //muestra el resultado
+                        Log.v("username: ", TextLogin.getText().toString());
+                        Log.v("contraseña: ", Textpassword.getText().toString());
+
+                    }
+                }
+        );
     }
 }

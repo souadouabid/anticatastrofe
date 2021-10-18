06bf10c225e013de30ea4d55d5fc0112f750e3be
Pep <josep.gascon.quintana@estudiantat.upc.edu>
2021-10-18 19:38:12 +0200

    comit nova branca maps

--- app/src/main/java/com/app/login/MainActivity.java
+++ app/src/main/java/com/app/login/MainActivity.java
@@ -1,13 +1,26 @@
 package com.app.login;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mapa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapa = (Button)findViewById(R.id.mapa);
+        mapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

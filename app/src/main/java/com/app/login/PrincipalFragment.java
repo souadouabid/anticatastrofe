34ce3433ebab0fbb70591022c229cb0789e889fe
Pep <josep.gascon.quintana@estudiantat.upc.edu>
2021-10-21 20:15:30 +0200

    maps activity afegit

--- app/src/main/java/com/app/login/PrincipalFragment.java
+++ app/src/main/java/com/app/login/PrincipalFragment.java
@@ -1,5 +1,6 @@
 package com.app.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +61,16 @@ public class PrincipalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_principal, container, false);
+        View v = inflater.inflate(R.layout.fragment_principal, container, false);
+        Button b = (Button) v.findViewById(R.id.buttonmapa);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), MapsActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+        return v;
     }
 }

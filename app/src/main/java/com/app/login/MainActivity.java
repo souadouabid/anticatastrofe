5011949f2841a095a62ecf599d504b6500638cd6
oscar <oscar.canabate@estudiantat.upc.edu>
2021-10-30 17:50:50 +0100

    xomendata una dependencia que hacia petar la app

--- app/src/main/java/com/app/login/MainActivity.java
+++ app/src/main/java/com/app/login/MainActivity.java
@@ -1,25 +1,14 @@
 package com.app.login;
-import androidx.appcompat.app.AppCompatActivity;
-import android.location.Location;
-import androidx.core.app.ActivityCompat;
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import com.app.Managers.Client;
-import org.json.JSONException;
-import java.io.IOException;
-import com.app.inicio.Inicio;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import com.google.android.gms.location.FusedLocationProviderClient;
+//import com.google.android.gms.location.LocationServices;
 
 
 public class MainActivity extends AppCompatActivity {

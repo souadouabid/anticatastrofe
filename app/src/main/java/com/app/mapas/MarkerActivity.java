fbb306ee879283c92fce7cf853974622336cc695
Pep <josep.gascon.quintana@estudiantat.upc.edu>
2021-11-22 19:19:50 +0100

    colors etiq

--- app/src/main/java/com/app/mapas/MarkerActivity.java
+++ app/src/main/java/com/app/mapas/MarkerActivity.java
@@ -9,8 +9,11 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +34,10 @@ public class MarkerActivity extends AppCompatActivity {
 
     private Float latitude;
     private Float longitude;
+    private Integer id_m;
+
+    private Spinner spinner;
+    private static final String[] paths = {"Red", "Yellow", "Green", "Cian", "Blue", "Pink"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +56,44 @@ public class MarkerActivity extends AppCompatActivity {
                         + "Long : " + longitude,
                 Toast.LENGTH_LONG).show();
 
+        System.out.println(id_m);
+        id_m = (int)(Math.random()*100000);
+        id_m *= 10;
+        System.out.println(id_m);
         Button ButtonCreaEtiq = (Button) findViewById(R.id.btn_crea_etiq);
         Button ButtonCancel = (Button) findViewById(R.id.btn_cancel);
 
         EditText TextTitol = (EditText) findViewById(R.id.titol);
         EditText TextDescripcio = (EditText) findViewById(R.id.desc);
+
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(MarkerActivity.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view,
+                                       int position, long id) {
+                Object item = adapterView.getItemAtPosition(position);
+                if (item != null) {
+                    Toast.makeText(MarkerActivity.this, item.toString(),
+                            Toast.LENGTH_SHORT).show();
+                            id_m += position;
+                }
+                Toast.makeText(MarkerActivity.this, "Selected",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
 
@@ -63,14 +103,13 @@ public class MarkerActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
 
-                    int id = (int)(Math.random()*100000);
-                    id *= 10;
-
                     String titol = TextTitol.getText().toString();
                     String desc = TextDescripcio.getText().toString();
 
                     try {
-                        Client.createLandmarkPep(id, latitude, longitude, titol, desc);
+                        System.out.println(id_m);
+
+                        Client.createLandmarkPep(id_m, latitude, longitude, titol, desc);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -87,5 +126,45 @@ public class MarkerActivity extends AppCompatActivity {
         });
     }
 
+/*
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                break;
+            case 1:
+                id_m += 1;
+                break;
+            case 2:
+                id_m += 2;
+                break;
+            case 3:
+                id_m += 3;
+                break;
+            case 4:
+                id_m += 4;
+                System.out.println(id_m);
+
+                break;
+            case 5:
+                id_m += 5;
+                break;
+            case 6:
+                id_m += 6;
+                break;
+            case 7:
+                id_m += 7;
+                break;
+            case 8:
+                id_m += 8;
+                break;
+
+        }
+    }  */
+
+
+
 
 }
+
+

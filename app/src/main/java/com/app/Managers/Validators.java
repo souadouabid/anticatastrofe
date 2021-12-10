07a3502c951e6f1cc078bb5e382450e9318c1701
rubenmontagut <ruben.pinell01@gmail.com>
2021-12-10 11:54:26 +0100

    comprovar email i numero al fer el register

---
+++ app/src/main/java/com/app/Managers/Validators.java
@@ -0,0 +1,32 @@
+package com.app.Managers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validators {
+
+    public static Boolean validateEmail (String email) {
+        //retorna el contrari per tema de les funcions al Registro.java
+        //retorna false si esta be el mail i retorna true si esta malament el mail
+        String emailRegex =  "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+        Pattern emailPat = Pattern.compile(emailRegex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = emailPat.matcher(email);
+        Boolean bool = !matcher.find();
+        System.out.println(bool);
+        return bool;
+    }
+
+    public static boolean validatePhone (String phone) {
+        //retorna el contrari per tema de les funcions al Registro.java
+        //retorna false si esta be el numero i retorna true si esta malament el numero
+        int num;
+        try {
+            num = Integer.parseInt(phone);
+            if(9 == phone.length()) return false;
+            else return true;
+
+        } catch (NumberFormatException e) {
+            return true;
+        }
+    }
+}

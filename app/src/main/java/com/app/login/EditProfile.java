10578d8cd396f5a8d5e9816216a28a7dcd075c93
rubenmontagut <ruben.pinell01@gmail.com>
2022-01-04 19:12:43 +0100

    pantalla perfil, falta acabar lo de date

---
+++ app/src/main/java/com/app/login/EditProfile.java
@@ -0,0 +1,14 @@
+package com.app.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile2);
+    }
+}
